5365e3fd256bf401b1e31927267d81291b8b9547
Aysenur Bilgin <aysenurbilgin@gmail.com>
2018-06-30 00:00:36 +0200

    Modified celery_app.py for broker configuration on dockerized version

--- flask/src/celery_tasks/celery_app.py
+++ flask/src/celery_tasks/celery_app.py
@@ -6,7 +6,7 @@ import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
 celery_app = Celery('src.celery_tasks',
-             # broker='amqp://newsgac:1234@rabbit//',
-             broker='amqp://newsgac:1234@localhost/newsgac_vhost',
+             broker='amqp://newsgac:1234@rabbit//',
+             # broker='amqp://newsgac:1234@localhost/newsgac_vhost',
              backend='rpc://',
              include=['src.celery_tasks.tasks'])
