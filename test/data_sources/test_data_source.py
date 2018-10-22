a7265957859f61ea445344bcc2a54436e091790b
Tom Klaver <tomklav@gmail.com>
2018-10-22 09:52:23 +0200

    update test_data_source test (fix endpoint name)

--- test/data_sources/test_data_source.py
+++ test/data_sources/test_data_source.py
@@ -7,7 +7,7 @@ from newsgac.data_sources.tasks import process
 def test_get_data_source(app, client, data_source_balanced_100):
     client.login()
     data_source = data_source_balanced_100
-    url = url_for('data_sources.get_data_source_page', data_source_id=data_source._id)
+    url = url_for('data_sources.view', data_source_id=data_source._id)
     sv = client.get(url)
     assert sv.status_code == 200
     assert is_valid_html(sv.data)
@@ -17,7 +17,7 @@ def test_get_data_source_stats(client, data_source_balanced_100):
     client.login()
     data_source = data_source_balanced_100
     process(data_source._id)
-    url = url_for('data_sources.get_data_source_page', data_source_id=data_source._id)
+    url = url_for('data_sources.view', data_source_id=data_source._id)
     sv = client.get(url)
     assert sv.status_code == 200
     assert is_valid_html(sv.data)
