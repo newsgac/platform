2bc8d11c35a3a673e2f6e44b81090712a0139683
Tom Klaver <tomklav@gmail.com>
2018-11-02 12:55:03 +0100

    add multi-layer perceptron classifier

--- newsgac/learners/__init__.py
+++ newsgac/learners/__init__.py
@@ -4,6 +4,7 @@ from newsgac.learners.models.grid_search import GridSearch
 from newsgac.learners.models.rf import LearnerRF
 from newsgac.learners.models.gb import LearnerGB
 from newsgac.learners.models.xgb import LearnerXGB
+from newsgac.learners.models.mlp import LearnerMLP
 
 learners = [
     LearnerNB,
@@ -11,5 +12,6 @@ learners = [
     LearnerRF,
     LearnerGB,
     LearnerXGB,
+    LearnerMLP,
     GridSearch
 ]
--- newsgac/learners/models/mlp.py
+++ newsgac/learners/models/mlp.py
@@ -1,20 +1,40 @@
 from pymodm import fields, EmbeddedMongoModel
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
 
 from .learner import Learner
 
 
 class Parameters(EmbeddedMongoModel):
-    alpha = fields.FloatField(default=0.1)
+    alpha = fields.FloatField(default=0.0001)
 
 
-class LearnerNB(Learner):
-    name = 'Naive Bayes'
-    tag = 'nb'
+class LearnerMLP(Learner):
+    name = 'Multi-Layer Perceptron'
+    tag = 'mlp'
     parameters = fields.EmbeddedDocumentField(Parameters)
 
     def get_classifier(self):
-        return MultinomialNB(alpha=self.parameters.alpha)
-
-    def get_features_weights(self):
-        return self.trained_model.steps[-1][1].coef_
+        return MLPClassifier(
+            hidden_layer_sizes = (100,),
+            activation = "relu",
+            solver = 'adam',
+            alpha = self.parameters.alpha,
+            batch_size = 'auto',
+            learning_rate = "constant",
+            learning_rate_init = 0.001,
+            power_t = 0.5,
+            max_iter = 200,
+            shuffle = True,
+            random_state = None,
+            tol = 1e-4,
+            verbose = False,
+            warm_start = False,
+            momentum = 0.9,
+            nesterovs_momentum = True,
+            early_stopping = False,
+            validation_fraction = 0.1,
+            beta_1 = 0.9,
+            beta_2 = 0.999,
+            epsilon = 1e-8,
+            n_iter_no_change = 10
+        )
