0932c4f4c9077d8797c6a4f72792ce78315af1a4
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-10-25 17:58:07 +0200

    fixing codacy issues

--- jupyter/entrypoint.sh
+++ jupyter/entrypoint.sh
@@ -3,7 +3,7 @@
 useradd "${JUPYTER_USER}"
 echo "${JUPYTER_USER}:${JUPYTER_PASSWORD}" | chpasswd
 mkdir "/home/${JUPYTER_USER}"
-chown "${JUPYTER_USER}:${JUPYTER_USER}" /home/${JUPYTER_USER}
+chown "${JUPYTER_USER}:${JUPYTER_USER}" "/home/${JUPYTER_USER}"
 chown "${JUPYTER_USER}:${JUPYTER_USER}" /notebooks
 
 exec "$@"
